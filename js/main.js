d5c2e18be33f7136aadf7f057e3d9b5e48df71a7
Xun (Peggie) Zhong <56562224+peggiezx@users.noreply.github.com>
2025-04-01 02:49:19 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// main.js
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Web Spreadsheet is loaded!');
+    // Later, you’ll write logic to create the grid, handle cell clicks, etc.
+  });
+  
